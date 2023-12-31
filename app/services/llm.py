2757e0f35230d83186c84a0bbe728bf3b62a4e38
Sven Kroll <s.kroll@crowdfox.com>
2023-12-31 13:10:22 +0100

    create the vectordb once

--- app/services/llm.py
+++ app/services/llm.py
@@ -15,8 +15,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 class LLMManager:
     def __init__(self, app):
-        self.create_vectordb()
-
+        self.vector_db = self.create_vectordb()
         self.qa_chain = None
         self.app = app
 
@@ -31,15 +30,7 @@ class LLMManager:
         set_debug(debug)
 
         try:
-            db_path = os.path.join(os.getcwd(), 'db')
-
-            # model_kwargs = {'device': 'cuda' if torch.cuda.is_available() else 'cpu'}
-            model_kwargs = {'device': 'cpu'}
-            encode_kwargs = {'normalize_embeddings': True}
-            embedding = HuggingFaceBgeEmbeddings(model_name='thenlper/gte-base', model_kwargs=model_kwargs,
-                                                 encode_kwargs=encode_kwargs)
-            vectordb = Chroma(persist_directory=db_path, embedding_function=embedding)
-            retriever = vectordb.as_retriever(search_kwargs={'k': 10})
+            retriever = self.vector_db.as_retriever(search_kwargs={'k': 10})
             prompt_template = PromptTemplate.from_template(self.app.config['PROMPT_TEMPLATE'])
             chain_type_kwargs = {'verbose': verbose, 'prompt': prompt_template}
             self.qa_chain = RetrievalQA.from_chain_type(llm=self.llm,
@@ -69,9 +60,8 @@ class LLMManager:
             encode_kwargs = {'normalize_embeddings': True}
             embedding = HuggingFaceBgeEmbeddings(model_name='thenlper/gte-base', model_kwargs=model_kwargs,
                                                  encode_kwargs=encode_kwargs)
-            vectordb = Chroma.from_documents(documents=texts,
+            return Chroma.from_documents(documents=texts,
                                              embedding=embedding,
                                              persist_directory=db_path)
-            logging.info(f"The '{db_path}' folder has been created.")
         except Exception as e:
             logging.error(f"An error occurred while processing file: {e}")
